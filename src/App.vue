d97516c8f64f74ca91e1a76d5bfc757a8545d66f
jfrs <jfrs@jfrs.me>
2023-12-30 11:16:49 +1100

    Init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  import Test from '@/Test.vue';
+</script>
+
+<template>
+  <Test wrong-prop />
+  <ImaginaryComponent />
+</template>
---
+++ src/Test.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+  defineProps<{ requiredString: string }>();
+</script>
+
+<template>
+  {{ requiredString }}
+</template>
